189900f7768145b7abcf674165cffe98014d7df7
chenguoyuan <1621712872@qq.com>
2020-12-06 11:17:26 +0800

    更新了login.js文件

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,7 +13,14 @@ $(function () {
   //从layui中获取对象
   var form = layui.form
   form.verify({
-    pwd: [/^[\S]{6,12}$/, 
-    '密码必须6到12位，且不能出现空格']
+    pwd: [/^[\S]{6,12}$/,
+      '密码必须6到12位，且不能出现空格'],
+    repwd: function (value) {
+      /* 通过形参拿到的是确认密码框中的值,去比较两个密码框中的值是否相等,然后进行判断 */
+      var pwd = $(".reg-box [name=password]").val()
+      if (pwd !== value) {
+        return ("两次密码不一致")
+      }
+    }
   })
 })
