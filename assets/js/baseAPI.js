a8d957e4714dd4f5efcc4265bdbd55ddd5b2a366
chenguoyuan <1621712872@qq.com>
2020-12-07 11:34:04 +0800

    优化了ajax请求代码

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,17 @@
 $.ajaxPrefilter(function (options) {
   options.url = "http://ajax.frontend.itheima.net" + options.url
-
+  if ((options.url).includes("/my/")) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || ''
+    }
+  }
+  //无论请求成功与否，都会执行complete属性
+  //complete是为了防止未登录的情况下可以访问index页面
+  options.complete = function (res) {
+    console.log(res);
+    if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+      localStorage.removeItem("token")
+      location.replace('/login.html')
+    }
+  }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,18 @@
 $(function () {
   getUserinfo()
+  $("#btnLogout").on("click", function () {
+    //提示用户是否退出
+    let layer = layui.layer
+    layer.confirm('确定是否退出', { icon: 3, title: '提示' },
+      function (index) {
+        //清空本地存储的token,为了防止每次登录的时候都加入了一个token本地存储
+        localStorage.removeItem("token")
+        //使用replace阻止浏览器记录历史记录
+        location.replace("/login.html")
+        //当没有代码执行的时候无法关闭提示窗口
+        layer.close(index);
+      });
+  })
 })
 //获取用户基本信息
 function getUserinfo() {
@@ -7,28 +20,25 @@ function getUserinfo() {
     method: "get",
     url: "/my/userinfo",
     //请求头配置对象
-    headers: {
-      Authorization: localStorage.getItem("token") || ''
-    },
     success: function (res) {
       if (res.status !== 0) {
         return layer.msg("请求失败")
       } else {
-        console.log(res);
         renderAvatar(res.data)
       }
-    }
+    },
+ 
   })
 }
 function renderAvatar(user) {
-  let name = user.nikename||user.username
-  $("#welcome").html("欢迎"+name)
-  if(!user.user_pic){
+  let name = user.nikename || user.username
+  $("#welcome").html("欢迎" + name)
+  if (!user.user_pic) {
     let first = name[0].toUpperCase()
     $(".layui-nav-img").hide()
     $(".text-avatar").html(first).show()
-  }else{
-    $(".layui-nav-img").attr("src",user.user_pic).show()
+  } else {
+    $(".layui-nav-img").attr("src", user.user_pic).show()
     $(".text-avatar").hide()
   }
 }
